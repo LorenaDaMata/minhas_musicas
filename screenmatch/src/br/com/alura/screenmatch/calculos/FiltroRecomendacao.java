e63dd9b3c43f16ebbbcd5930890065d86ff682ad
Lorena da Mata <lorenakarolineapareida@gmail.com>
2024-02-17 15:51:17 -0300

    filtro recomendação 4

--- screenmatch/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
+++ screenmatch/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -3,9 +3,9 @@ package br.com.alura.screenmatch.calculos;
 public class FiltroRecomendacao {
 	
 	public void filtra(Classificavel classificavel) {
-		if (classificavel.getClassificacao() >= 5) {
+		if (classificavel.getClassificacao() >= 3) {
 			System.out.println("Está entre os preferidos do momento");
-		} else if (classificavel.getClassificacao() >= 2) {
+		} else if (classificavel.getClassificacao() >= 1) {
 			System.out.println("Muito bem avaliado no momento!");
 		} else {
 			System.out.println("Coloque na sua lista para assistir depois");
